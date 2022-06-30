f8067a61ce29bc1b2b46d188f5b6f6fc71f00932
Kojo Appiedu <benadkojo@gmail.com>
2022-06-30 11:42:40 +0000

    authentication

--- base/views.py
+++ base/views.py
@@ -2,11 +2,23 @@ from dataclasses import fields
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView , UpdateView
+from django.views.generic.edit import CreateView , UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Task 
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = 'all'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+     return reverse_lazy('tasks')
+     
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
@@ -25,3 +37,9 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url: reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
