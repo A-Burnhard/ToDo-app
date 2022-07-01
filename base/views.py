cc390715778395b7e3a28e2eb911f9336a6ec72c
Kojo Appiedu <benadkojo@gmail.com>
2022-07-01 15:17:23 +0000

    templates updated

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from dataclasses import fields
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView , UpdateView, DeleteView, FormView
@@ -26,6 +26,17 @@ class RegisterPage(FormView):
     redirect_authenticated_user = True
     success_url = reverse_lazy('tasks')
 
+    def form_invalid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage,self).form_valid(form)
+
+    def get(self, *args, **kwargs): 
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args,**kwargs)
+
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
@@ -33,7 +44,13 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] =context['tasks'].filter(user=self.request.user)
-        context['count'] =context['tasks'].filter(complete=False)
+        context['count'] =context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains = search_input
+            )
         return context
 
 class TaskDetail(LoginRequiredMixin, DetailView):
